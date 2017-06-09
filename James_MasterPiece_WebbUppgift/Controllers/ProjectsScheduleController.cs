da4f487871432702da69431dc46352d6d94a8393
James Johansson <shyco_2000@hotmail.com>
2017-06-09 18:07:19 +0200

    lägger in projekt schema årsvis

---
+++ James_MasterPiece_WebbUppgift/Controllers/ProjectsScheduleController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JMP_WU_Domain;
+using James_MasterPiece_WebbUppgift.Context;
+
+namespace James_MasterPiece_WebbUppgift.Controllers
+{
+    public class ProjectsScheduleController : Controller
+    {
+        private readonly MasterPieceContext _context;
+
+        public ProjectsScheduleController()
+        {
+            _context = new MasterPieceContext();    
+        }
+
+        // GET: ProjectsSchedule
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Project.ToListAsync());
+        }
+        
+    }
+}
